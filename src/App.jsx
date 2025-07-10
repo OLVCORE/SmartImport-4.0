ed16ad720d1e0007de10111c7b791cc94ab3df7c
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-07-09 23:33:05 -0300

    debug: App.jsx renderizando apenas Header para isolar erro fatal em produção

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import React from 'react'
 import Header from './components/Layout/Header'
-import Sidebar from './components/Layout/Sidebar'
-import Dashboard from './pages/Dashboard'
 
-console.log('[SmartImport] App.jsx montando')
+console.log('[SmartImport] App.jsx montando (só Header)')
 
 function App() {
-  // Etapa 1: Renderizar só Header
-  // return <Header />
-
-  // Etapa 2: Renderizar Header + Sidebar
-  // return <div className="flex"><Sidebar /><div className="flex-1"><Header /></div></div>
-
-  // Etapa 3: Renderizar Header + Sidebar + Dashboard
-  return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1">
-        <Header />
-        <main className="p-8">
-          <Dashboard />
-        </main>
-      </div>
-    </div>
-  )
+  return <Header />
 }
 
 export default App 
