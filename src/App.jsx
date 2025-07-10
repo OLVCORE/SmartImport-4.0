21dbd25a5bd3eee9bd970d49ee5cf2621cbc3432
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-07-09 23:43:45 -0300

    debug: App.jsx renderizando Header + Sidebar para diagnóstico incremental

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Header from './components/Layout/Header'
+import Sidebar from './components/Layout/Sidebar'
 
-console.log('[SmartImport] App.jsx montando (só Header)')
+console.log('[SmartImport] App.jsx montando (Header + Sidebar)')
 
 function App() {
-  return <Header />
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+      </div>
+    </div>
+  )
 }
 
 export default App 
